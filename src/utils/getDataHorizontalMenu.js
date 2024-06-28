6226671c2519b16f2acf007d58864c668eef0177
Juan Antonio <jamll.muro@gmail.com>
2024-06-28 10:19:02 +0200

    Add a new object array that returns all the info of the horizontal menu

---
+++ src/utils/getDataHorizontalMenu.js
@@ -0,0 +1,44 @@
+const getDataHorizontalMenu = [
+  {
+    id: '01',
+    image: '/assets/jams.webp',
+    title: 'B/D® JAMS',
+    subtitle: 'It\'s a vibe',
+    description: 'A weekly-ish playlist curated by the employees @BASIC/DEPT®',
+    year: '2022',
+  },
+  {
+    id: '02',
+    image: '/assets/a001.webp',
+    title: 'APPLIED®',
+    subtitle: 'Thoughts & perspectives',
+    description: 'Our panel series and thought-leadership platform where we share perspectives and tactics related to strategy, design, and technology.',
+    year: '2020',
+  },
+  {
+    id: '03',
+    image: '/assets/moves.webp',
+    title: 'MOVES®',
+    subtitle: 'Our new hq',
+    description: 'When we moved into our new HQ, we put together a site experience providing an in-depth look into the two-year process imagining, designing, and developing our new office we call home.',
+    year: '2019',
+  },
+  {
+    id: '04',
+    image: '/assets/culture_collision.webp',
+    title: 'CRAFTED®',
+    subtitle: 'Creative community',
+    description: 'A communal initiative we operate to celebrate diversity and creativity by bringing together people from different backgrounds and interests.',
+    year: '2019',
+  },
+  {
+    id: '05',
+    image: '/assets/brandbeats.webp',
+    title: 'BRANDBEATS®',
+    subtitle: 'Podcast series',
+    description: 'Our podcast series garnering 45,000+ listens per episode providing candid conversations around various industry related topics.',
+    year: '2017',
+  },
+]
+
+export default getDataHorizontalMenu;
