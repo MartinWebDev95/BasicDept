82d60d3fb700e680b4b3ec0c63658d8c69e871a5
Juan Antonio <jamll.muro@gmail.com>
2025-04-29 09:43:16 +0200

    Add a new file that return the featured news data

---
+++ src/utils/getDataFeaturedNews.js
@@ -0,0 +1,70 @@
+const getDataFeaturedNews = [
+  {
+    id: 1,
+    image: '/assets/featured_news_1.webp',
+    title: 'FOUR NOMINATIONS FOR THE INTERNET’S HIGHEST HONOR.',
+    press: '4.16.25',
+  },
+  {
+    id: 2,
+    image: '/assets/featured_news_2.webp',
+    title: 'Tomorrow’s shopper: Five ways brands can reach Gen Alpha today',
+    press: '10.11.24',
+  },
+  {
+    id: 3,
+    image: '/assets/featured_news_3.webp',
+    title: 'Generation Alpha: Say hello to tomorrow’s shopper ',
+    press: '7.22.24',
+  },
+  {
+    id: 4,
+    image: '/assets/featured_news_4.webp',
+    title: 'The Future of Fashion and AI at Glossy\'s Ecommerce Summit',
+    press: '6.13.24',
+  },
+  {
+    id: 6,
+    image: '/assets/featured_news_5.webp',
+    title: 'Retail’s new playground: where physical meets digital',
+    press: '6.3.24',
+  },
+  {
+    id: 7,
+    image: '/assets/featured_news_6.webp',
+    title: 'Gen Alpha, the beta test for how brands can reach a new generation',
+    press: '5.28.24',
+  },
+  {
+    id: 8,
+    image: '/assets/featured_news_7.webp',
+    title: 'New Projects on the Podium for the 28th Webby Awards',
+    press: '4.24.24',
+  },
+  {
+    id: 9,
+    image: '/assets/featured_news_8.webp',
+    title: 'What Sofia Coppola Can Teach Us About Shifting Culture',
+    press: '4.18.24',
+  },
+  {
+    id: 10,
+    image: '/assets/featured_news_9.webp',
+    title: 'The 28TH Annual Webby Awards Nods Are In',
+    press: '4.4.24',
+  },
+  {
+    id: 11,
+    image: '/assets/featured_news_10.webp',
+    title: 'Are snacks the new meals in QSR?',
+    press: '3.20.24',
+  },
+  {
+    id: 12,
+    image: '/assets/featured_news_11.webp',
+    title: 'From Ordinary to Extraordinary: Bringing the In-Store Luxury Experience Online',
+    press: '3.6.24',
+  },
+];
+
+export default getDataFeaturedNews;
