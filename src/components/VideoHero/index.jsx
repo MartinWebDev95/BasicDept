e44f13b2efd5d48ac31d9211bc9f06cccd0ff30e
Juan Antonio <jamll.muro@gmail.com>
2025-05-09 10:23:35 +0200

    Add a new VideoHero component where it is displayed the trailer and the complete video

---
+++ src/components/VideoHero/index.jsx
@@ -0,0 +1,60 @@
+import formatTime from "../../helpers/formatTime";
+import styles from "./VideoHero.module.css";
+import useVideoPlayer from "../../hooks/useVideoPlayer";
+
+const VideoHero = () => {
+
+  const { 
+    isVideoHidden, 
+    duration, 
+    currentTime, 
+    videoRef, 
+    overlayRef, 
+    containerRef,
+    handlePlay,
+    handlePause,
+    handleMouseDown,
+    handleMouseMove,
+    handleMouseUp 
+  } = useVideoPlayer();
+
+  return (
+    <>
+      <video 
+        preload='metadata' 
+        src="/assets/hero_video_trailer.mp4"
+        autoPlay 
+        loop
+        muted 
+        className={styles.trailer}
+        onClick={handlePlay} 
+      >
+      </video>
+
+      <div className={styles.video_container} aria-hidden={isVideoHidden}>
+        <video 
+          ref={videoRef}
+          preload='metadata' 
+          src="/assets/hero_video.mp4"
+          autoPlay 
+          className={styles.video} 
+          onClick={handlePause}
+        >
+        </video>
+
+        <div ref={containerRef} className={styles.video_overlay}>
+          <p 
+            ref={overlayRef}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+            onMouseUp={handleMouseUp}
+          >
+            {`${formatTime(currentTime)}/${formatTime(duration)}`}
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default VideoHero;
