a5383fbad8866950abab0a12e41aaee1cf3ca802
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-19 11:12:03 +0200

    Add a new FeaturesBrandCarousel component to show a list of the brands which can be horizontal scrolled

---
+++ src/components/FeaturesBrandCarousel/index.jsx
@@ -0,0 +1,65 @@
+import { useRef } from "react";
+import useMoveCursor from "../../hooks/useMoveCursor";
+import getDataBrandFeatures from "../../utils/getDataBrandFeatures";
+import CustomCursor from "../CustomCursor";
+import styles from './FeaturesBrandCarousel.module.css';
+
+const FeaturesBrandCarousel = () => {
+
+  const barRef = useRef();
+  const progressbarRef = useRef();
+
+  const handleScroll = (e) => {
+    const scrollX = e.target.scrollLeft;
+    
+    progressbarRef.current.style.left = `${scrollX / 1.63}px`;
+  }
+  
+  const { 
+    cursorRef,
+    carouselRef,
+    isPressed, 
+    handleMouseEnter, 
+    handleMouseMove, 
+    handleMouseLeave,
+    handleMouseDown,
+    handleMouseUp, 
+  } = useMoveCursor();
+
+  return (
+    <>
+      <div 
+        className={styles.carousel} 
+        onMouseEnter={handleMouseEnter}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      >
+        <ol ref={carouselRef} className={styles.carousel_list} onScroll={handleScroll}>
+          {getDataBrandFeatures.map((item) => (
+            <li key={item.id} className={styles.carousel_item}>
+              <div className={styles.img_container}>
+                <img src={item.image} alt={item.title} />
+              </div>
+              
+              <div className={styles.info}>
+                <h2>{item.title}</h2>
+                <p className={styles.description}>{item.description}</p>
+              </div>
+            </li>
+          ))}
+        </ol>
+        
+        <CustomCursor cursorRef={cursorRef} isPressed={isPressed} type="carousel" />
+      </div>
+
+      <div ref={barRef} className={styles.carousel_progress}>
+        <span ref={progressbarRef} className={styles.carousel_progressbar}></span>
+      </div>
+    </>
+  )
+}
+
+export default FeaturesBrandCarousel;
+
