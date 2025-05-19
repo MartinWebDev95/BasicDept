53cc2389eb7f067de4bcd18137317d9c7ba9072d
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-19 09:17:15 +0200

    Add a new file that return an array with info about the different brands

---
+++ src/utils/getDataBrandFeatures.js
@@ -0,0 +1,34 @@
+const getDataBrandFeatures = [
+  {
+    id: '01',
+    image: '/assets/google_logo.webp',
+    title: 'Google',
+    description: 'Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions.',
+  },
+  {
+    id: '02',
+    image: '/assets/kfc_logo.webp',
+    title: 'KFC',
+    description: 'An award-winning global, digital transformation engagement spanning eCommerce, mobile app, and new drive thru experiences. Bringing KFC’s brand story to life while making it easier for customers to buy chicken.',
+  },
+  {
+    id: '03',
+    image: '/assets/wilson_logo.webp',
+    title: 'Wilson',
+    description: 'A reimagining of Wilson’s brand visual identity, and brand campaign, to support a new product drop and the launch of the first brick and mortar retail location in the brand’s 108-year history.',
+  },
+  {
+    id: '04',
+    image: '/assets/at&t_logo.webp',
+    title: 'AT&T',
+    description: 'Redesigning the digital flagship for the largest telecommunications company in the world. Creating frictionless paths to purchase for a wide range of consumers across a vast portfolio of products and services.',
+  },
+  {
+    id: '05',
+    image: '/assets/patagonia_logo.webp',
+    title: 'Patagonia',
+    description: 'Ongoing partnership providing strategy, branding, experience design, and development focused on bringing their mission and offerings to consumers through brand-led programs and platforms.',
+  },
+]
+
+export default getDataBrandFeatures;
