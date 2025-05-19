d6a815151c4c83daad350cfa01ac3472b37195fe
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-19 11:07:48 +0200

    Change the useMoveCursor custom hook to get a better movement

--- src/hooks/useMoveCursor.js
+++ src/hooks/useMoveCursor.js
@@ -1,22 +1,52 @@
 import { useRef, useState } from "react";
 
 let cursorSize = 0;
+let coordX = 0;
 
 const useMoveCursor = () => {
   const [isPressed, setIsPressed] = useState(false);
 
   const cursorRef = useRef(null);
+  const carouselRef = useRef(null);
+  const startX = useRef(0);
+  const scrollStartRef = useRef(0);
 
   const handleMouseEnter = () => {
     cursorSize = cursorRef.current.offsetWidth;
   }
 
   const handleMouseMove = (e) => {
-    const mouseX = (e.clientX - 96) - (cursorSize + (cursorSize / 2));
-    const mouseY = e.clientY - (cursorSize + (cursorSize / 2));
-    
+    const mouseX = e.clientX;
+    const mouseY = e.clientY;
+    // const mouseX = e.clientX - (cursorSize * 2);
+    // const mouseY = e.clientY - (cursorSize * 2);
+
     cursorRef.current.style.transition = 'none';
-    cursorRef.current.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    cursorRef.current.style.transform = `translate3d(${mouseX - (cursorSize * 2)}px, ${mouseY - (cursorSize * 2)}px, 0)`;
+
+    // console.log({mouseX: mouseX + cursorSize, coordX});
+    // if(isPressed && (mouseX >= coordX)){
+      
+    //   carouselRef.current.style.transition = 'all .3s linear';
+    //   carouselRef.current.scrollLeft -= `${mouseX - coordX}`;
+    // }
+
+    // if(isPressed && (mouseX <= coordX)){
+      
+    //   // console.log({mouseX, coordX});
+    //   carouselRef.current.style.transition = 'all .3s linear';
+    //   carouselRef.current.scrollLeft += `${coordX - mouseX}`;
+    // }
+    if(isPressed){
+      // const deltaX = previousX.current - mouseX;
+      // console.log({previousX: previousX.current, mouseX, deltaX});
+      // carouselRef.current.scrollLeft += deltaX;
+      const walk = mouseX - startX.current; // how far the mouse has moved
+      carouselRef.current.scrollLeft = scrollStartRef.current - walk; // scroll the element
+      console.log({mouseX, startX: startX.current, scrollStart: scrollStartRef.current, walk});
+    }
+
+    // previousX.current = mouseX;
   }
   
   const handleMouseLeave = () => {
@@ -25,9 +55,14 @@ const useMoveCursor = () => {
     setIsPressed(false);
   }
   
-  const handleMouseDown = () => {
+  const handleMouseDown = (e) => {
     setIsPressed(true);
-    cursorSize = cursorRef.current.offsetWidth;
+    // coordX = e.clientX;
+    // console.log(coordX);
+    // cursorSize = cursorRef.current.offsetWidth;
+    // previousX.current = e.clientX;
+    startX.current = e.clientX;
+    scrollStartRef.current = carouselRef.current.scrollLeft;
   }
   
   const handleMouseUp = () => {
@@ -36,6 +71,7 @@ const useMoveCursor = () => {
 
   return {
     cursorRef,
+    carouselRef,
     isPressed,
     handleMouseEnter,
     handleMouseMove,
