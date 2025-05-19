60a99b1cce81b30047de93ef9cdc1f2b802f4d0f
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-19 10:16:33 +0200

    Change the Navigation component to add a new item in the navbar, a new icon and change the overflow of the page depending if menu is opened or closed

--- src/components/Navigation/index.jsx
+++ src/components/Navigation/index.jsx
@@ -2,16 +2,44 @@ import { useState } from 'react';
 import styles from './Navigation.module.css';
 import HorizontalMenu from '../HorizontalMenu';
 
+export const ArrowIcon = () => {
+  return (
+    <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"  stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-arrow-right">
+      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+      <path d="M5 12l14 0" />
+      <path d="M13 18l6 -6" />
+      <path d="M13 6l6 6" />
+    </svg>
+  )
+}
+
 const Navigation = () => {
 
   const [isMenuHidden, setIsMenuHidden] = useState(true);
   const [isHorizontalMenuHidden, setIsHorizontalMenuHidden] = useState(true);
 
   const handleHorizontalMenu = () => {
+    if(window.innerWidth > 1200 && isHorizontalMenuHidden) {
+      document.querySelector('body').style.overflow = 'hidden';
+    }
+    
+    if(window.innerWidth > 1200 && !isHorizontalMenuHidden) {
+      document.querySelector('body').style.overflow = 'auto';
+    }
+
     setIsHorizontalMenuHidden(!isHorizontalMenuHidden);
   }
 
   const handleMenu = () => {
+    if(isMenuHidden) {
+      document.querySelector('body').style.overflow = 'hidden';
+      document.querySelector('body').style.setProperty('--colorLogo', '#F9CDCD');
+      document.querySelector('header').classList.remove('hide_header');
+    } else {
+      document.querySelector('body').style.overflow = 'auto';
+      document.querySelector('body').style.setProperty('--colorLogo', '#fff');
+    }
+
     setIsMenuHidden(!isMenuHidden);
   }
 
@@ -36,6 +64,12 @@ const Navigation = () => {
           </li>
           <li className={styles.list_item}>
             <a href="#">Contact</a>
+          </li>
+          <li className={styles.list_item}>
+            <button type='button' onClick={handleHorizontalMenu}>
+              Initiatives
+              <ArrowIcon />
+            </button>
           </li>
         </ul>
 
