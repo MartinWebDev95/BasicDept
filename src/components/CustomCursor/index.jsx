a8bd68bb24408fd5976ac017cf76e45c13efebfb
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-19 10:27:32 +0200

    Change the CustomCursor component to add a new param to differentiate between the cursor of horizontal menu and the cursor of the carousel brands

--- src/components/CustomCursor/index.jsx
+++ src/components/CustomCursor/index.jsx
@@ -1,11 +1,12 @@
 import styles from './CustomCursor.module.css';
 
-const CustomCursor = ({ cursorRef, isPressed }) => {
+const CustomCursor = ({ cursorRef, isPressed, type }) => {
   return (
     <button 
       type="button" 
       ref={cursorRef}
       aria-pressed={isPressed}
+      data-cursor={type}
       className={styles.cursor}
     >
       Drag
