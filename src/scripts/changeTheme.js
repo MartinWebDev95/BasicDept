46e63d36247fdfc93424df037a69b9aa1add50bf
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-20 10:53:40 +0200

    Change the changeTheme scripts to update the color of the logo

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -12,12 +12,22 @@ const observer = new IntersectionObserver((entries) => {
       document.querySelector('body').style.setProperty('--textColor', '#F9CDCD');
       document.querySelector('body').style.setProperty('--bgColor', '#252422');
       document.querySelector('body').style.setProperty('--bgBar', '#5E5E5E');
-      document.querySelector('body').style.setProperty('--colorLogo', '#F9CDCD');
+
+      // Remove the animation to change the color of the logo
+      document.querySelector('.logo').style.animation = 'none';
+      document.querySelector('.logo').style.fill = '#F9CDCD';
     } else {
       document.querySelector('body').style.setProperty('--textColor', '#252422');
       document.querySelector('body').style.setProperty('--bgColor', '#ECECEC');
       document.querySelector('body').style.setProperty('--bgBar', '#B4B4B4');
-      document.querySelector('body').style.setProperty('--colorLogo', '#252422');
+
+      // Restore the animation
+      document.querySelector('.logo').style.animation = 'changeColorLogo linear both';
+      document.querySelector('.logo').style.animationTimeline = 'scroll()';
+      document.querySelector('.logo').style.animationRange = '200px 500px';
+      
+      // Restore the original color of the logo
+      document.querySelector('.logo').style.fill = '';
     }
   });
 }, options);
