c13ac675c6a78ebb4b9fc65b05c3a7219fb8cd2e
Juan Antonio Martín <jamll.muro@gmail.com>
2025-05-21 10:30:18 +0200

    Change the hideHeader script to not hide the header when the menu mobile is opened

--- src/scripts/hideHeader.js
+++ src/scripts/hideHeader.js
@@ -1,10 +1,25 @@
 let lastScroll = 0;
+let isMenuOpen = false;
 const header = document.querySelector('header');
+const menuMobile = document.querySelector('#menu_mobile');
+
+// Function to avoid the header to hide when the menu is open
+menuMobile.addEventListener('click', () => {
+  isMenuOpen = !isMenuOpen;
+
+  if(isMenuOpen) {
+    header.classList.remove('hide_header');
+  }
+});
 
 window.addEventListener('scroll', () => {
+  // If the menu is open, do not hide the header
+  if(isMenuOpen) return;
+  
   const currentScroll = window.scrollY;
 
   if (currentScroll > lastScroll) {
+    console.log('hola');
     header.classList.add('hide_header');
   } else {
     header.classList.remove('hide_header');
